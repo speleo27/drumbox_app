61199970687c27e7d1cf41157ea91eb224760a7e
speleo27 <seb10400@orange.fr>
2021-08-10 07:24:23 +0200

    first commit work on theme color wip header footer

---
+++ src/components/header/index.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default function Header(){
+    return(
+        <Wrapper>
+            <h1>Our super music app</h1>
+        </Wrapper>
+    );
+}
+const Wrapper = styled.header``;
