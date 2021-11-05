c0d9ce9d1628dd5db18ae38b1fb6c9e830ca6d62
speleo27 <seb10400@orange.fr>
2021-11-05 13:59:25 +0100

    end of project add possibility to change sounds work on presentation

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,13 +5,16 @@ export default function About(){
         return (
             <Wrapper>
                     <h2>About</h2>
-
+                    <div>
+                        <p>Actuellement en reconversion professionnelle, je suis déjà titulaire d'un titre pro de développeur web et web-mobile </p>
+                    </div>
             </Wrapper>
         );
 
 
 }
 const Wrapper = styled.div``;
-const h2 =styled.h2`
+const p = styled.div`
+margin-top: 10px;
 text-align: center;
 `;
